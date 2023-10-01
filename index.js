06513d005595ddf80f31bfac6db9b24d1467ba6f
zhambalov <aazhambalov@gmail.com>
2023-10-01 02:11:14 -0400

    added "contact", "about", "portfolio"

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ document.getElementById("portfolio").onclick = function () {
     window.open("portfolio.html");
 }
 
+document.getElementById("contact").onclick = function () {
+    window.open("contact.html");
+}
+
+document.getElementById("about").onclick = function () {
+    window.open("about.html");
+}
+
 document.getElementById("portfolio").style.cursor = "pointer";
+document.getElementById("contact").style.cursor = "pointer";
+document.getElementById("about").style.cursor = "pointer";
 
 const portfolioText = document.getElementById("portfolio");
 
