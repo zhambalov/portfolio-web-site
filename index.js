09286fe56625155c8af56db29ea68dc303e5ecc2
zhambalov <aazhambalov@gmail.com>
2023-09-30 23:10:03 -0400

    1.0.2

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById("portfolio").onclick = function () {
+    window.open("portfolio.html");
+}
+
+document.getElementById("portfolio").style.cursor = "pointer";
+
+const portfolioText = document.getElementById("portfolio");
+
+
+
+
