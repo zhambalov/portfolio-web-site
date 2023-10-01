9f585791af1f428a39c93af40550edfb0a3f53a4
zhambalov <aazhambalov@gmail.com>
2023-10-01 05:11:07 -0400

    added portfolio, resume and skills

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ document.getElementById("about").onclick = function () {
     window.open("about.html");
 }
 
+
 document.getElementById("portfolio").style.cursor = "pointer";
 document.getElementById("contact").style.cursor = "pointer";
 document.getElementById("about").style.cursor = "pointer";
 
-const portfolioText = document.getElementById("portfolio");
+
 
 
 
