ee525ccc29a948662bb413330f5aea2a1adfadec
zhambalov <aazhambalov@gmail.com>
2023-10-01 11:30:26 -0400

    FINAAAAL!!

---
+++ routes/index.js
@@ -0,0 +1,38 @@
+/* 
+   File: index.js
+   Student: Aldar Zhambalov
+   StudentID: 301275745
+   Date: September 30, 2023
+*/
+
+
+var express = require('express');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home Page' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Me' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Me' });
+});
+
+/* GET portfolio page. */
+router.get('/portfolio', function(req, res, next) {
+  res.render('portfolio', { title: 'My Portfolio' });
+});
+
+/* GET skills page. */
+router.get('/skills', function(req, res, next) {
+  res.render('skills', { title: 'My skills' });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,22 @@
+// Load Node modules
+var express = require('express');
+const ejs = require('ejs');
+// Initialise Express
+var app = express();
+// Render static files
+app.use(express.static('public'));
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+app.listen(3000);
+
+// Root Route
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+app.get('/about', function (req, res) {
+    res.render('about'); 
+  });
+
+const routes = require('./routes'); // Update the path accordingly
+app.use('/', routes);
