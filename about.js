e451bc433c6312022bb54666c89c1ac56f85a05f
zhambalov <aazhambalov@gmail.com>
2023-10-01 08:35:49 -0400

    Final Version!

--- about.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("skills").onclick = function () {
-    window.open("skills.html");
-}
-
-document.getElementById("skills").style.cursor = "pointer";
-document.getElementById("resume").style.cursor = "pointer";
-
---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+const ejs = require('ejs');
+
+var app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.listen(8080);
